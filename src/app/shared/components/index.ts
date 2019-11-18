475961a012b3fbc4b4c679e3abe08d12b93bdf52
¨Zawada <zawada.sylwiaa@gmail.com>
2019-11-18 23:41:57 +0100

    Import new components - filters, mobileMenu

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,10 +1,15 @@
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
-
+import { FiltersComponent } from './filters/filters.component';
+import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 
 export const SharedComponents = [
-  NavigationMenuComponent
+  NavigationMenuComponent,
+  FiltersComponent,
+  MobileMenuComponent
 ];
 
 export {
-  NavigationMenuComponent
+  NavigationMenuComponent,
+  FiltersComponent,
+  MobileMenuComponent
 };
