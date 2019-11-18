30e50dde386daa98684f032f1cb91266d3eabb3f
¨Zawada <zawada.sylwiaa@gmail.com>
2019-11-18 23:39:52 +0100

    Add hostlistener - resize window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  innerWidth: any;
+  isDesktopWidth: boolean;
+  desktopWidthView = 1024;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth >= this.desktopWidthView) {
+      this.isDesktopWidth = true;
+    } else {
+      this.isDesktopWidth = false;
+    }
+    // console.log(this.innerWidth);
+    // console.log(this.isDesktopWidth);
+  }
+  constructor() {
+    this.onResize();
+  }
 }
