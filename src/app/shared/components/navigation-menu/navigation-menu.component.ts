3f934be2f0cee1db745f2687b3cb436e811bc446
¨Zawada <zawada.sylwiaa@gmail.com>
2019-11-18 23:58:55 +0100

    Add breakpointObserver variable

--- src/app/shared/components/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/components/navigation-menu/navigation-menu.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { AppRouterUrls } from '../../../app-routing.config';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation-menu',
@@ -9,6 +12,12 @@ import { AppRouterUrls } from '../../../app-routing.config';
 export class NavigationMenuComponent {
 
   appRouterUrls = AppRouterUrls;
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 
-  constructor() {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
 }
