06f015f9ddb3dd38205d0ae062a19585f4bf2e85
¨Zawada <zawada.sylwiaa@gmail.com>
2019-11-18 23:33:57 +0100

    Import material modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 // in this module we import every angular material module
 @NgModule({
   imports: [
     CommonModule,
-    MatSliderModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
+    MatListModule,
+    LayoutModule
   ],
   exports: [
-    CommonModule,
-    MatSliderModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
+    MatListModule,
+    LayoutModule
   ]
 })
 export class MaterialModule { }
