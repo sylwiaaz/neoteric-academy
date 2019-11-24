521e9288e443f51038de26f5ca2432ceb5311548
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 21:07:01 +0100

    Import BrandsViewComponent

---
+++ src/app/views/brands/components/index.ts
@@ -0,0 +1,9 @@
+import { BrandsViewComponent } from '../components/brands-view/brands-view.component';
+
+export const BrandsComponents = [
+  BrandsViewComponent
+];
+
+export {
+  BrandsViewComponent
+};
