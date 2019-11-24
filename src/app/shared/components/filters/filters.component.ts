2ef07f5913d9e7a6df1f2783044eb6277806902e
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 20:37:53 +0100

    Add new variable - allPlace

--- src/app/shared/components/filters/filters.component.ts
+++ src/app/shared/components/filters/filters.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
@@ -7,16 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltersComponent implements OnInit {
   onShow = false;
+
   selectedPlace = 'All';
-  langs = ['All', 'JS', 'Java', 'HTML', 'PHP', 'Ruby', 'Python', 'DevOps', '.Net', 'C++', 'Scala', 'Mobile', 'Testing'];
-
   places = ['All', 'Warszawa', 'Kraków', 'Wrocław', 'Poznań', 'Trójmiasto', 'Remote', 'World'];
+  // tslint:disable-next-line: max-line-length
   otherPlaces = ['Białystok', 'Bielsko-Biała', 'Bydgoszcz', 'Częstochowa', 'Gliwice', 'Katowice', 'Kielce', 'Lublin', 'Łódź', 'Olsztyn', 'Opole', 'Toruń', 'Rzeszów', 'Szczecin'];
+  allPlace = [...this.places, ...this.otherPlaces];
 
-  constructor() {
+  constructor() { }
 
-  }
+  ngOnInit() { }
 
-  ngOnInit() {
-  }
 }
