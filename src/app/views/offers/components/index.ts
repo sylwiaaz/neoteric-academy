b288d09b84ff40a38cc3931221c0983efd72b973
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 21:06:13 +0100

    import OffersViewComponent

---
+++ src/app/views/offers/components/index.ts
@@ -0,0 +1,12 @@
+import { OffersViewComponent } from '../components/offers-view/offers-view.component';
+// import { OffersMobileViewComponent } from './offers-mobile-view/offers-mobile-view.component';
+
+export const OffersComponents = [
+  OffersViewComponent
+  // OffersMobileViewComponent
+];
+
+export {
+  OffersViewComponent
+  // OffersMobileViewComponent
+};
