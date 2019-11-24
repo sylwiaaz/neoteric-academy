c25d0638d13517be5f178fd0fe5a4a70908ff4a4
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 21:19:25 +0100

    Add resizeSubscription

--- src/app/views/brands/components/brands-view/brands-view.component.ts
+++ src/app/views/brands/components/brands-view/brands-view.component.ts
@@ -1,35 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ResizeService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-brands-view',
   templateUrl: './brands-view.component.html',
   styleUrls: ['./brands-view.component.scss']
 })
-export class BrandsViewComponent implements OnInit {
-typeBrands = [
-{
-  type: 'Startup',
-  icon: 'whatshot'
-},
-{
-    type: 'Software House',
-    icon: 'house'
-}, {
-  type: 'E-commerce',
-  icon: 'shopping_basket'
-},
-{
-  type: 'Corporation',
-  icon: 'business'
-},
-{
-  type: 'Other',
-  icon: 'tv'
-}
-];
-  constructor() { }
+export class BrandsViewComponent implements OnInit, OnDestroy {
+  typeBrands = [
+    {
+      type: 'All'
+    },
+    {
+      type: 'Startup',
+      icon: 'whatshot'
+    },
+    {
+      type: 'Software House',
+      icon: 'house'
+    }, {
+      type: 'E-commerce',
+      icon: 'shopping_basket'
+    },
+    {
+      type: 'Corporation',
+      icon: 'business'
+    },
+    {
+      type: 'Other',
+      icon: 'tv'
+    }
+  ];
+  selectedBrand = 'All';
+
+  public isDesktopWidth = window.innerWidth > 1024 ? true : false;
+  private resizeSubscription: Subscription;
+
+  constructor(private resizeService: ResizeService) {
+    this.resizeSubscription = this.resizeService.onResize$
+    .subscribe(size => {
+      if (size.innerWidth > 1024) {
+        this.isDesktopWidth = true;
+      } else {
+        this.isDesktopWidth = false;
+      }
+      console.log(this.isDesktopWidth);
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
   }
 
 }
