65f6fbfd0ea1cbbca86b75d9eb39bf5dbe381fac
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 20:27:26 +0100

    Import filtersMobileComponent

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,15 +1,18 @@
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { FiltersComponent } from './filters/filters.component';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
+import { FiltersMobileComponent } from './filters-mobile/filters-mobile.component';
 
 export const SharedComponents = [
   NavigationMenuComponent,
   FiltersComponent,
-  MobileMenuComponent
+  MobileMenuComponent,
+  FiltersMobileComponent
 ];
 
 export {
   NavigationMenuComponent,
   FiltersComponent,
-  MobileMenuComponent
+  MobileMenuComponent,
+  FiltersMobileComponent
 };
