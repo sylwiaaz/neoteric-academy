616c0a0d739cc87c3e1290eed527bd804ff7a985
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-11-24 19:07:11 +0100

    Add  path to default, register, brands

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
+import { BrandsViewComponent } from './views/brands/components/index';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppRouterUrls, AppRoutes } from './app-routing.config';
-import {AuthLoginComponent} from './views/auth/components';
+import { AuthLoginComponent, AuthRegisterComponent} from './views/auth/components';
+import { OffersViewComponent } from './views/offers/components';
 
 const routes: Routes = [
   // odkomentować gdy dodasz komponent offers
-  // { path: '', redirectTo: AppRouterUrls.DEFAULT, pathMatch: 'full' },
+  { path: '', redirectTo: AppRouterUrls.DEFAULT, pathMatch: 'full' },
   {
     path: AppRoutes.AUTH,
     children: [
       { path: '', pathMatch: 'full', redirectTo: AppRouterUrls.LOGIN },
-      { path: AppRoutes.LOGIN, component: AuthLoginComponent }
+      { path: AppRoutes.LOGIN, component: AuthLoginComponent },
+      { path: AppRoutes.REGISTER, component: AuthRegisterComponent }
     ]
-  }
+  },
+  { path: AppRoutes.BRANDS, component: BrandsViewComponent },
+  { path: AppRoutes.DEFAULT, component: OffersViewComponent },
 ];
 
 @NgModule({
