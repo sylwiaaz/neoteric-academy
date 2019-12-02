60fdd35ad2e50dd0bd36d433792067ed9ae5b1ce
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-02 15:25:59 +0100

    Create offersList component

---
+++ src/app/views/offers/components/offers-list/offers-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { OfferService } from '../../services';
+
+@Component({
+  selector: 'app-offers-list',
+  templateUrl: './offers-list.component.html',
+  styleUrls: ['./offers-list.component.scss']
+})
+export class OffersListComponent implements OnInit {
+  offers;
+  constructor(private offerService: OfferService) { }
+
+  ngOnInit() {
+    this.offers = this.offerService.getOffers();
+  }
+
+}
