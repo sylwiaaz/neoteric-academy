8c5de76b4267e510702288a56cb6d9745d3b3339
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-02 15:05:24 +0100

    Import leafletMap component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,17 +2,20 @@ import { NavigationMenuComponent } from './navigation-menu/navigation-menu.compo
 import { FiltersComponent } from './filters/filters.component';
 import { MobileMenuComponent } from './mobile-menu/mobile-menu.component';
 import { FiltersMobileComponent } from './filters-mobile/filters-mobile.component';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
 
 export const SharedComponents = [
   NavigationMenuComponent,
   FiltersComponent,
   MobileMenuComponent,
-  FiltersMobileComponent
+  FiltersMobileComponent,
+  LeafletMapComponent
 ];
 
 export {
   NavigationMenuComponent,
   FiltersComponent,
   MobileMenuComponent,
-  FiltersMobileComponent
+  FiltersMobileComponent,
+  LeafletMapComponent
 };
