4b5f623b09882e44d25089c3177961f81bb53be4
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-02 15:27:18 +0100

    Create offersMap component

---
+++ src/app/views/offers/components/offers-map/offers-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offers-map',
+  templateUrl: './offers-map.component.html',
+  styleUrls: ['./offers-map.component.scss']
+})
+export class OffersMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
