2fb427d8d9517d061313211c0ebc728f547c75e9
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-02 15:07:30 +0100

    Create leafletMap component

---
+++ src/app/shared/components/leaflet-map/leaflet-map.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-leaflet-map',
+  templateUrl: './leaflet-map.component.html',
+  styleUrls: ['./leaflet-map.component.scss']
+})
+export class LeafletMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    const map = L.map('map').setView([54.4, 18.5], 10);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
+}
