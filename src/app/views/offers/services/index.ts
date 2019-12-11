9c2651a791b3534557d21c1998204b57ad5c90a7
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-12 00:45:02 +0100

    Import FilterService

--- src/app/views/offers/services/index.ts
+++ src/app/views/offers/services/index.ts
@@ -1,12 +1,15 @@
+import { FilterService } from './filter.service';
 import { OfferService } from './offer-service.service';
 import { MapService } from './map.service';
 
 export const OffersServices = [
   OfferService,
-  MapService
+  MapService,
+  FilterService
 ];
 
 export {
   OfferService,
-  MapService
+  MapService,
+  FilterService
 };
