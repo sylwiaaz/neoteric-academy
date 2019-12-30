a8284f61a497aee3fc37c76e7b24d94db9c8eee4
sylwiaaz <zawada.sylwiaa@gmail.com>
2019-12-30 15:47:27 +0100

    Import user & httpClient modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UserModule } from './views/user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -9,19 +10,22 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './views/auth/auth.module';
 import { BrandsModule } from './views/brands/brands.module';
 import { OffersModule } from './views/offers/offers.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
     SharedModule,
     AuthModule,
     BrandsModule,
-    OffersModule
+    OffersModule,
+    UserModule
   ],
   providers: [
     CoreGuards
