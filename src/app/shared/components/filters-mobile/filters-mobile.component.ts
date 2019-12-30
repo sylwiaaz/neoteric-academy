01b749382c280af49af203c59f94502fd2eb3160
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 12:05:14 +0100

    Keep selected place in session storage

--- src/app/shared/components/filters-mobile/filters-mobile.component.ts
+++ src/app/shared/components/filters-mobile/filters-mobile.component.ts
@@ -1,4 +1,3 @@
-import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FilterService } from 'src/app/views/offers/services';
 
@@ -11,16 +10,22 @@ export class FiltersMobileComponent implements OnInit {
   selectedPlace: string;
   places: string[];
 
-  constructor(private router: Router, private filterService: FilterService) {
+  constructor(private filterService: FilterService) {
     this.places = this.filterService.allPlaces;
     this.selectedPlace = this.filterService.selectedPlace;
   }
 
   ngOnInit() {
+    if (sessionStorage.getItem('selectedPlace') === null) {
+      this.selectedPlace = 'All';
+    } else {
+      this.selectedPlace = JSON.parse(sessionStorage.getItem('selectedPlace'));
+    }
   }
 
   onFilterPlace(place) {
-      this.filterService.selectedPlace = place;
-      this.filterService.onFilter();
+    this.filterService.selectedPlace = place;
+    sessionStorage.setItem('selectedPlace', JSON.stringify(place));
+    this.filterService.onFilter();
   }
 }
