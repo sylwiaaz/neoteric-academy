52cc9f9f12aae1411fb9232ac93808122c653897
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 11:25:34 +0100

    Change login fn, add auth status subscription

--- src/app/views/auth/components/auth-login/auth-login.component.ts
+++ src/app/views/auth/components/auth-login/auth-login.component.ts
@@ -1,30 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../services';
 import { Router } from '@angular/router';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-auth-login',
   templateUrl: './auth-login.component.html',
   styleUrls: ['./auth-login.component.scss'],
 })
-export class AuthLoginComponent implements OnInit {
+export class AuthLoginComponent implements OnInit, OnDestroy {
   hide = true;
-  signUpForm: FormGroup;
+  signInForm: FormGroup;
+  authStatusSub: Subscription;
+  isLogged = false;
   constructor(private authService: AuthService, private router: Router) { }
+
   onNavigateToRegister() {
     this.router.navigate(['/auth/register']);
-  }
+    }
 
   ngOnInit() {
-    this.signUpForm = new FormGroup( {
+    this.signInForm = new FormGroup( {
       email: new FormControl(null, [Validators.required,
     Validators.email]),
     password: new FormControl(null, [Validators.required,  Validators.minLength(6) ])
     });
+    this.authStatusSub = this.authService.getAuthStatus().subscribe(
+      isAuth => {
+        this.isLogged = isAuth;
+      }
+    );
   }
 
   onLogin() {
-    // console.log('ok');
+    if (this.signInForm.invalid) {
+      return;
+    }
+    this.authService.login(this.signInForm.value.email, this.signInForm.value.password);
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
