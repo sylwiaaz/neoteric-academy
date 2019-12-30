067a114c0e89218ae85f5a9ce54b36b92f93e82d
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 12:10:10 +0100

    Impoer user dashboard components

---
+++ src/app/views/user/components/index.ts
@@ -0,0 +1,21 @@
+import { UserPreferencesComponent } from './user-panel/user-preferences/user-preferences.component';
+import { UserSettingsComponent } from './user-panel/user-settings/user-settings.component';
+import { UserMatchmakingComponent } from './user-panel/user-matchmaking/user-matchmaking.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
+import { UserProfileComponent } from './user-panel/user-profile/user-profile.component';
+
+export const UserComponents = [
+  UserPanelComponent,
+  UserProfileComponent,
+  UserMatchmakingComponent,
+  UserSettingsComponent,
+  UserPreferencesComponent
+];
+
+export {
+  UserPanelComponent,
+  UserProfileComponent,
+  UserMatchmakingComponent,
+  UserSettingsComponent,
+  UserPreferencesComponent
+};
