cd49444396f84001c9c5143f8341085316a40a8c
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 11:45:52 +0100

    Create user matchmaking component

---
+++ src/app/views/user/components/user-panel/user-matchmaking/user-matchmaking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-matchmaking',
+  templateUrl: './user-matchmaking.component.html',
+  styleUrls: ['../user-profile/user-profile.component.scss']
+})
+export class UserMatchmakingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
