6a1cc5d3147962464b548d3e8e7bce333f13ab10
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 11:44:19 +0100

    Create user preferences component

---
+++ src/app/views/user/components/user-panel/user-preferences/user-preferences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-preferences',
+  templateUrl: './user-preferences.component.html',
+  styleUrls: ['../user-profile/user-profile.component.scss']
+})
+export class UserPreferencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
