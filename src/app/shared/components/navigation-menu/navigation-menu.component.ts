9ae96aae084e5a9928b4b7f47b467faa0e8ddb61
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 11:21:41 +0100

    Add user info box

--- src/app/shared/components/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/components/navigation-menu/navigation-menu.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AppRouterUrls } from '../../../app-routing.config';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../../../views/auth/services';
 
 @Component({
   selector: 'app-navigation-menu',
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss'],
 })
-export class NavigationMenuComponent {
-
+export class NavigationMenuComponent implements OnInit, OnDestroy {
+  isLogged: boolean;
+  isOpenUserDropdown = false;
   appRouterUrls = AppRouterUrls;
+  authStatusSub: Subscription;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {  }
 
+  ngOnInit() {
+    this.isLogged = this.authService.getIsAuth();
+    this.authStatusSub = this.authService.getAuthStatus().subscribe(
+      isAuth => {
+        this.isLogged = isAuth;
+      }
+    );
+  }
+  onLogOut() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
   }
 }
