614751a00d91809054f133f4bfa05d29999fc955
Sylwia Zawada <zawada.sylwiaa@gmail.com>
2019-12-30 11:47:33 +0100

    Create Offer model

---
+++ src/app/views/offers/services/offer.model.ts
@@ -0,0 +1,17 @@
+export interface Offer {
+  _id: number;
+  jobTitle: string;
+  tech: string;
+  salary: string;
+  date: string;
+  companyName: string;
+  companyAddress: string;
+  companyPlace: string;
+  skills: string[];
+  logoPath: string;
+  companySize: string;
+  employmentType: string;
+  experienceLevel: string;
+  techStack: string[];
+  location: number[];
+}
