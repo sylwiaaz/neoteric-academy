f1d551e34d5d176ed48acf057c17fe9a6e64a1c1
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-04 22:59:57 +0100

    Add clearFilters & setFilters fns

--- src/app/views/offers/services/filter.service.ts
+++ src/app/views/offers/services/filter.service.ts
@@ -1,65 +1,223 @@
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AppRouterUrls } from '../../../app-routing.config';
+import { OfferService } from './offer-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class FilterService {
-
+  appRouterUrls = AppRouterUrls;
   assetsPath = '../../../../../assets/images/';
   technologies = [
-    { name: 'JS', icon: `${this.assetsPath}js.svg`, background: `linear-gradient(-90deg, #FFC706, #FFAF00)` },
-    { name: 'HTML', icon: `${this.assetsPath}html5.svg`, background: `linear-gradient(-90deg, #FF7D50, #FD5D21)` },
-    { name: 'PHP', icon: `${this.assetsPath}php.svg`, background: `linear-gradient(-90deg, #41ADFA, #157CFC)` },
-    { name: 'Ruby', icon: `${this.assetsPath}ruby.svg`, background: `linear-gradient(-90deg, #ef5350, #f44336)` },
-    { name: 'Python', icon: `${this.assetsPath}python.svg`, background: `linear-gradient(-90deg, #1F5EB6, #1F7BC4)` },
-    { name: 'Java', icon: `${this.assetsPath}java.svg`, background: `linear-gradient(-90deg, #FA656B, #F9597A)` },
-    { name: '.Net', icon: `${this.assetsPath}dotNet.png`, background: `linear-gradient(-90deg, #3FC5E6, #4FA4FD)` },
-    { name: 'Scala', icon: `${this.assetsPath}scala.png`, background: `linear-gradient(-90deg, #F86468, #F1464C)` },
-    { name: 'C++', icon: `${this.assetsPath}C++.svg`, background: `linear-gradient(-90deg, #2FCFBB, #37DD9C)` },
-    { name: 'Mobile', icon: `${this.assetsPath}mobile.svg`, background: `linear-gradient(-90deg, #E04F86, #BA4A8D)` },
-    { name: 'Testing', icon: `${this.assetsPath}testing.svg`, background: `linear-gradient(-90deg, #89DB54, #65AF35)` },
-    { name: 'DevOps', icon: `${this.assetsPath}devops.png`, background: `linear-gradient(-90deg, #5266E1, #8166E0)` },
-    { name: 'Game', icon: `${this.assetsPath}game.svg`, background: `linear-gradient(-90deg, #ff4081, #ec407a)` },
-    { name: 'UX/UI', icon: `${this.assetsPath}UX.svg`, background: `linear-gradient(-90deg, #ffb74d, #ff9800)` }];
+    {
+      name: 'JS',
+      icon: `${this.assetsPath}js.svg`,
+      background: `linear-gradient(-90deg, #FFC706, #FFAF00)`
+    },
+    {
+      name: 'HTML',
+      icon: `${this.assetsPath}html5.svg`,
+      background: `linear-gradient(-90deg, #FF7D50, #FD5D21)`
+    },
+    {
+      name: 'PHP',
+      icon: `${this.assetsPath}php.svg`,
+      background: `linear-gradient(-90deg, #41ADFA, #157CFC)`
+    },
+    {
+      name: 'Ruby',
+      icon: `${this.assetsPath}ruby.svg`,
+      background: `linear-gradient(-90deg, #ef5350, #f44336)`
+    },
+    {
+      name: 'Python',
+      icon: `${this.assetsPath}python.svg`,
+      background: `linear-gradient(-90deg, #1F5EB6, #1F7BC4)`
+    },
+    {
+      name: 'Java',
+      icon: `${this.assetsPath}java.svg`,
+      background: `linear-gradient(-90deg, #FA656B, #F9597A)`
+    },
+    {
+      name: '.Net',
+      icon: `${this.assetsPath}dotNet.png`,
+      background: `linear-gradient(-90deg, #3FC5E6, #4FA4FD)`
+    },
+    {
+      name: 'Scala',
+      icon: `${this.assetsPath}scala.png`,
+      background: `linear-gradient(-90deg, #F86468, #F1464C)`
+    },
+    {
+      name: 'C++',
+      icon: `${this.assetsPath}C++.svg`,
+      background: `linear-gradient(-90deg, #2FCFBB, #37DD9C)`
+    },
+    {
+      name: 'Mobile',
+      icon: `${this.assetsPath}mobile.svg`,
+      background: `linear-gradient(-90deg, #E04F86, #BA4A8D)`
+    },
+    {
+      name: 'Testing',
+      icon: `${this.assetsPath}testing.svg`,
+      background: `linear-gradient(-90deg, #89DB54, #65AF35)`
+    },
+    {
+      name: 'DevOps',
+      icon: `${this.assetsPath}devops.png`,
+      background: `linear-gradient(-90deg, #5266E1, #8166E0)`
+    },
+    {
+      name: 'Game',
+      icon: `${this.assetsPath}game.svg`,
+      background: `linear-gradient(-90deg, #ff4081, #ec407a)`
+    },
+    {
+      name: 'UX/UI',
+      icon: `${this.assetsPath}UX.svg`,
+      background: `linear-gradient(-90deg, #ffb74d, #ff9800)`
+    }
+  ];
   otherTechnologies = [
-    { name: 'PM', icon: `${this.assetsPath}PM.svg`, background: `linear-gradient(-90deg, #80cbc4, #4db6ac)` },
-    { name: 'Blockchain', icon: `${this.assetsPath}blockchain.svg`, background: `linear-gradient(-90deg, #AB47BC, #6A1B9A)` },
-    { name: 'Security', icon: `${this.assetsPath}security.svg`, background: `linear-gradient(-90deg, #536DFE, #0D47A1)` },
-    { name: 'Data', icon: `${this.assetsPath}data.svg`, background: `linear-gradient(-90deg, #009688, #00796B)` },
-    { name: 'Golang', icon: `${this.assetsPath}golang.png`, background: `linear-gradient(-90deg, #6AD7E5, #6A8BE5)` },
-    { name: 'SAP', icon: `${this.assetsPath}sap.svg`, background: `linear-gradient(-90deg, #02AFEB, #1B68C2)` },
-    { name: 'Other', icon: `${this.assetsPath}devices.svg`, background: `linear-gradient(-90deg, #EC4CB6, #D44BD5)` }];
-  allTechnologies = ['All', ...this.technologies.map(tech => tech.name), ...this.otherTechnologies.map(tech => tech.name)];
+    {
+      name: 'PM',
+      icon: `${this.assetsPath}PM.svg`,
+      background: `linear-gradient(-90deg, #80cbc4, #4db6ac)`
+    },
+    {
+      name: 'Blockchain',
+      icon: `${this.assetsPath}blockchain.svg`,
+      background: `linear-gradient(-90deg, #AB47BC, #6A1B9A)`
+    },
+    {
+      name: 'Security',
+      icon: `${this.assetsPath}security.svg`,
+      background: `linear-gradient(-90deg, #536DFE, #0D47A1)`
+    },
+    {
+      name: 'Data',
+      icon: `${this.assetsPath}data.svg`,
+      background: `linear-gradient(-90deg, #009688, #00796B)`
+    },
+    {
+      name: 'Golang',
+      icon: `${this.assetsPath}golang.png`,
+      background: `linear-gradient(-90deg, #6AD7E5, #6A8BE5)`
+    },
+    {
+      name: 'SAP',
+      icon: `${this.assetsPath}sap.svg`,
+      background: `linear-gradient(-90deg, #02AFEB, #1B68C2)`
+    },
+    {
+      name: 'Other',
+      icon: `${this.assetsPath}devices.svg`,
+      background: `linear-gradient(-90deg, #EC4CB6, #D44BD5)`
+    }
+  ];
+  allTechnologies = [
+    'All',
+    ...this.technologies.map(tech => tech.name),
+    ...this.otherTechnologies.map(tech => tech.name)
+  ];
 
-  appRouterUrls = AppRouterUrls;
-  selectedPlace = 'All';
-  selectedTech = 'All';
-  selectedExp = 'all';
-  selectedMinSal = '0k';
-  selectedMaxSal = '51k';
-  places = ['All', 'Warszawa', 'Kraków', 'Wrocław', 'Poznań', 'Trójmiasto', 'Remote', 'World'];
-  // tslint:disable-next-line: max-line-length
-  otherPlaces = ['Białystok', 'Bielsko-Biała', 'Bydgoszcz', 'Częstochowa', 'Gliwice', 'Katowice', 'Kielce', 'Lublin', 'Łódź', 'Olsztyn', 'Opole', 'Toruń', 'Rzeszów', 'Szczecin'];
+  places = [
+    'All',
+    'Warszawa',
+    'Kraków',
+    'Wrocław',
+    'Poznań',
+    'Trójmiasto',
+    'Remote',
+    'World'
+  ];
+  otherPlaces = [
+    'Białystok',
+    'Bielsko-Biała',
+    'Bydgoszcz',
+    'Częstochowa',
+    'Gliwice',
+    'Katowice',
+    'Kielce',
+    'Lublin',
+    'Łódź',
+    'Olsztyn',
+    'Opole',
+    'Toruń',
+    'Rzeszów',
+    'Szczecin'
+  ];
   allPlaces = [...this.places, ...this.otherPlaces];
 
-  constructor(private router: Router) { }
+  selectedPlace: string;
+  selectedTech: string;
+  selectedExp: string;
+  selectedMinSal: string;
+  selectedMaxSal: string;
 
-  onFilter() {
-    this.selectedTech = sessionStorage.getItem('selectedTech') === null ? 'All' : JSON.parse(sessionStorage.getItem('selectedTech'));
+  constructor(private router: Router, private offerService: OfferService) {
+    this.router.events.subscribe(events => {
+      if (events instanceof NavigationEnd) {
+
+        const urlParams = decodeURIComponent(this.router.url).split('/');
+        console.log(urlParams);
+        const [slashSeg, offerSegment, placeSeg, techSeg, expSeg, minSalSeg, maxSalSeg] = urlParams;
+        if (urlParams.length === 2 && offerSegment === 'offers') {
+          this.offerService.getOffers();
+        }
+        this.onFilter(placeSeg, techSeg, expSeg, minSalSeg, maxSalSeg);
+        this.setFilters(placeSeg, techSeg, expSeg, minSalSeg, maxSalSeg);
+      }
+    });
+  }
+
+  onFilter(place, tech?, exp?, minSal?, maxSal?) {
+    if (maxSal) {
+      this.offerService.getOffersByAllFilters(place, tech, exp, minSal, maxSal);
+    } else {
+      if (minSal) {
+        this.offerService.getOffersByMinSal(place, tech, exp, minSal);
+      } else {
+        if (exp) {
+          this.offerService.getOffersByExp(place, tech, exp);
+        } else {
+          if (tech) {
+            this.offerService.getOffersByPlaceAndTech(place, tech);
+          } else {
+            if (place) {
+              this.offerService.getOffersByPlace(place);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  capitalize(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  }
+
+
+  onNavigateByFilter() {
     if (this.router.url.includes('offers')) {
       if (this.selectedMaxSal === '51k') {
         if (this.selectedMinSal === '0k') {
           if (this.selectedExp.toLowerCase() === 'all') {
             if (this.selectedTech.toLowerCase() === 'all') {
-              this.router.navigate([this.appRouterUrls.DEFAULT,
-              this.selectedPlace.toLowerCase()]);
+              this.router.navigate([
+                this.appRouterUrls.DEFAULT,
+                this.selectedPlace.toLowerCase()
+              ]);
             } else {
-              this.router.navigate([this.appRouterUrls.DEFAULT,
-              this.selectedPlace.toLowerCase(),
-              this.selectedTech.toLowerCase()]);
+              this.router.navigate([
+                this.appRouterUrls.DEFAULT,
+                this.selectedPlace.toLowerCase(),
+                this.selectedTech.toLowerCase()
+              ]);
             }
           } else {
             this.router.navigate([
@@ -90,5 +248,18 @@ export class FilterService {
       }
     }
   }
-}
 
+  setFilters(placeSeg?, techSeg?, expSeg?, minSalSeg?, maxSalSeg?) {
+    this.selectedPlace = placeSeg ? this.capitalize(placeSeg) : 'All';
+    this.selectedTech = techSeg ? this.capitalize(techSeg) : 'All';
+    this.selectedExp = expSeg ? expSeg : 'all';
+    this.selectedMinSal = minSalSeg ? minSalSeg : '0k';
+    this.selectedMaxSal = maxSalSeg ? maxSalSeg : '51k';
+  }
+
+  onClearFilters() {
+    this.router.navigateByUrl('/brands', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['/offers']);
+    });
+  }
+}
