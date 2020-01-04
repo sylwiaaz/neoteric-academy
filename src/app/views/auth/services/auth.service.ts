161fce7f7a770347ae5912b2673bd86de0c734a6
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-04 22:29:33 +0100

    Set cookie after login

--- src/app/views/auth/services/auth.service.ts
+++ src/app/views/auth/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -12,11 +13,17 @@ export class AuthService {
   private isAuthenticated = false;
   private authStatusListener = new Subject<boolean>();
   private serverPath = 'http://localhost:5000/';
-  public currentUser = '';
+  public currentUser;
+  private currentUserEmail;
 
   constructor(private http: HttpClient,
               private router: Router,
-              private snackBarService: SnackBarService) { }
+              private cookieService: CookieService,
+              private snackBarService: SnackBarService) {
+    if (this.cookieService.get('id_token')) {
+      this.isAuthenticated = true;
+    }
+  }
 
   getAuthStatus() {
     return this.authStatusListener.asObservable();
@@ -30,7 +37,6 @@ export class AuthService {
     const authData = { email, password };
     return this.http.post(`${this.serverPath}auth/register`, authData)
       .subscribe((response) => {
-        console.log(response);
         const message = 'Your account has been created succesfully. Please, login now.';
         this.snackBarService.showSuccess(message);
         this.router.navigate(['/auth/login']);
@@ -38,26 +44,28 @@ export class AuthService {
         if (error instanceof HttpErrorResponse) {
           const message = 'Account with this email address already exists';
           this.snackBarService.showError(message);
-          console.log(error.error.message);
         }
       });
   }
 
   login(email: string, password: string) {
     const authData = { email, password };
-    this.http.post<{ _id: number, email: string, password: string }>(`${this.serverPath}auth/login`, authData, { withCredentials: true })
+    this.http.post<any>(`${this.serverPath}auth/login`, authData, { withCredentials: true })
       .subscribe((response) => {
-        const user = response.email;
-        this.currentUser = user.substr(0, response.email.indexOf('@'));
+        // this.currentUserEmail = response.updatedUser.email;
+        // const value = response.updatedUser.token;
+
+        this.currentUserEmail = response.user.email;
+        this.currentUser = this.currentUserEmail.substr(0, this.currentUserEmail.indexOf('@'));
+        const value = response.tokenData.token;
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         this.router.navigate(['']);
+        this.cookieService.set('id_token', value, 0.041667, '/');
       }, error => {
         if (error instanceof HttpErrorResponse) {
           const message = 'Invalid email address or password';
           this.snackBarService.showError(message);
-          console.log(error);
-          console.log(error.error.message);
         }
       });
   }
@@ -65,11 +73,11 @@ export class AuthService {
   logout() {
     this.http.post(`${this.serverPath}auth/logout`, { withCredentials: true })
       .subscribe((response) => { },
-      error => {
-        this.isAuthenticated = false;
-        this.authStatusListener.next(false);
-        this.router.navigate(['']);
-      });
-
+        error => {
+          this.isAuthenticated = false;
+          this.cookieService.deleteAll('/');
+          this.authStatusListener.next(false);
+          this.router.navigate(['']);
+        });
   }
 }
