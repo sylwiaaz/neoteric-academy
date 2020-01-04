f4a9bdf2627810a8a91bdcf661683ee3671e465b
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-04 23:02:30 +0100

    Add get offers functions

--- src/app/views/offers/services/offer-service.service.ts
+++ src/app/views/offers/services/offer-service.service.ts
@@ -1,22 +1,76 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Offer } from './offer.model';
-
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
 
-  private offers: Offer[];
+  offers: Offer[];
+  private offersSubject = new Subject<Offer[]>();
+  private mainPath = 'http://localhost:5000/offers/';
 
   constructor(private http: HttpClient) { }
 
-  getOffers() {
-    return this.http.get<Offer[]>('http://localhost:5000/offers');
+  getOffersListener() {
+    return this.offersSubject.asObservable();
   }
 
-  getOffer(index: number) {
-     return this.offers[index];
+  getOffers() {
+    this.http.get<Offer[]>(this.mainPath)
+      .subscribe(offers => {
+        this.offers = offers;
+        this.offersSubject.next([...offers]);
+      });
+  }
+
+  getOffer(index: string) {
+    return this.http.get<Offer>(`${this.mainPath}offer/${index}`);
+  }
+
+  getOffersByPlace(place: string) {
+    return this.http.get<Offer[] | []>(`${this.mainPath}${place}`)
+      .subscribe(offers => {
+        this.setOffers(offers);
+      });
+  }
+
+  getOffersByPlaceAndTech(place: string, tech: string) {
+    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}`)
+      .subscribe(offers => {
+        this.setOffers(offers);
+      });
+  }
+
+  getOffersByExp(place: string, tech: string, exp: string) {
+    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}`)
+      .subscribe(offers => {
+        this.setOffers(offers);
+      });
+  }
+
+  getOffersByMinSal(place: string, tech: string, exp: string, minSal: string) {
+    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}/${minSal}`)
+      .subscribe(offers => {
+        this.setOffers(offers);
+      });
+  }
+
+  getOffersByAllFilters(place: string, tech: string, exp: string, minSal: string, maxSal: string) {
+    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}/${minSal}/${maxSal}`)
+      .subscribe(offers => {
+        this.setOffers(offers);
+      });
+  }
+
+  private setOffers(offers: Offer[] | []) {
+    this.offers = offers;
+    if (this.offers.length > 0) {
+      this.offersSubject.next([...this.offers]);
+    } else {
+      this.offersSubject.next([]);
+    }
   }
 }
