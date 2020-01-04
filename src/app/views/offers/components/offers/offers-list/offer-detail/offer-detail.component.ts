79caaf9dfcf600e069818692b85993774edee9ba
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-04 22:57:08 +0100

    Change navigateBack fn

--- src/app/views/offers/components/offers/offers-list/offer-detail/offer-detail.component.ts
+++ src/app/views/offers/components/offers/offers-list/offer-detail/offer-detail.component.ts
@@ -5,6 +5,7 @@ import { OfferService } from '../../../../services';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { AppRouterUrls } from '../../../../../../app-routing.config';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-offer-detail',
@@ -13,39 +14,36 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class OfferDetailComponent implements OnInit {
   offer: Offer;
-  id: number;
-  appRouterUrls = AppRouterUrls;
+  id: string;
   showMoreClauseInfo = false;
   displayFutureContsent = false;
   applyCVForm: FormGroup;
   isInvalidForm: boolean;
 
   constructor(private offerService: OfferService,
-              private router: Router,
               private route: ActivatedRoute,
-              private mapService: MapService) {
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.id = +params.id;
-        this.offerService.getOffers().subscribe(offers => {
-          this.offer = offers[this.id];
+              private mapService: MapService,
+              private location: Location) {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+      this.offerService.getOffer(this.id)
+        .subscribe(offer => {
+          this.offer = offer;
           this.mapService.zoomToPlace(this.offer.location);
         });
-      }
-    );
+    });
   }
 
   ngOnInit() {
     document.querySelector('.offers').scrollTop = 0;
     this.applyCVForm = new FormGroup({
       name: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required,
-      Validators.email])
+      email: new FormControl(null, [Validators.required, Validators.email])
     });
   }
 
   onNavigateBack() {
-    this.router.navigate([''], { relativeTo: this.route });
+    this.location.back();
     this.mapService.zoomOut();
   }
 
