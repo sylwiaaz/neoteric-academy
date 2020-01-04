6390e78cd8e51ba47ef01f81d18eaa8404be99a5
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-04 22:14:07 +0100

    Add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRouterUrls, AppRoutes } from './app-routing.config';
 import { AuthLoginComponent, AuthRegisterComponent } from './views/auth/components';
 import { OffersComponent, OfferDetailComponent, OffersListComponent } from './views/offers/components';
-import { IsAuthenticatedOnLoginGuard } from './guards';
+import { IsAuthenticatedOnLoginGuard, IsAuthenticatedGuard } from './guards';
 
 const routes: Routes = [
   // odkomentować gdy dodasz komponent offers
   { path: '', redirectTo: AppRouterUrls.DEFAULT, pathMatch: 'full' },
   {
-    path: AppRoutes.AUTH,
+    path: AppRoutes.AUTH, canActivate: [IsAuthenticatedGuard],
     children: [
       { path: '', pathMatch: 'full', redirectTo: AppRouterUrls.LOGIN },
-      { path: AppRoutes.LOGIN, component: AuthLoginComponent },
+      { path: AppRoutes.LOGIN, component: AuthLoginComponent, },
       { path: AppRoutes.REGISTER, component: AuthRegisterComponent },
-      // { path: AppRoutes.LOGOUT, component: AuthLoginComponent }
     ]
   },
   {
@@ -24,15 +23,13 @@ const routes: Routes = [
       { path: '', component: BrandsViewComponent },
     ]
   },
-  // canActivate: [IsAuthenticatedOnLoginGuard]
-   { path: AppRoutes.DASHBOARD, children: [
+   { path: AppRoutes.DASHBOARD, canActivate: [IsAuthenticatedOnLoginGuard], children: [
     { path: '', pathMatch: 'full', redirectTo: AppRouterUrls.PROFILE},
     { path: AppRoutes.PROFILE, component: UserProfileComponent},
     { path: AppRoutes.PREFERENCES, component: UserPreferencesComponent},
     { path: AppRoutes.SETTINGS, component: UserSettingsComponent},
     { path: AppRoutes.MATCHMAKING, component: UserMatchmakingComponent},
   ]
-
    },
   {
     path: AppRoutes.DEFAULT, component: OffersComponent, children: [
@@ -50,7 +47,7 @@ const routes: Routes = [
   }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
