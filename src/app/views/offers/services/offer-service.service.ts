36cadef76cd7ab84939d2ff61c4defcf8d7cbe58
Sylwia <zawada.sylwiaa@gmail.com>
2020-01-06 00:07:06 +0100

    Change get offers fns to one getOffersByFilter

--- src/app/views/offers/services/offer-service.service.ts
+++ src/app/views/offers/services/offer-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Offer } from './offer.model';
 import { Subject } from 'rxjs';
@@ -7,10 +7,10 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class OfferService {
-
   offers: Offer[];
-  private offersSubject = new Subject<Offer[]>();
+  offersSubject = new Subject<Offer[]>();
   private mainPath = 'http://localhost:5000/offers/';
+  errorMessage: string;
 
   constructor(private http: HttpClient) { }
 
@@ -19,58 +19,53 @@ export class OfferService {
   }
 
   getOffers() {
-    this.http.get<Offer[]>(this.mainPath)
-      .subscribe(offers => {
-        this.offers = offers;
-        this.offersSubject.next([...offers]);
-      });
+    this.http.get<Offer[]>(this.mainPath).subscribe(offers => {
+      this.offers = offers;
+      this.offersSubject.next(offers);
+    });
   }
 
   getOffer(index: string) {
     return this.http.get<Offer>(`${this.mainPath}offer/${index}`);
   }
 
-  getOffersByPlace(place: string) {
-    return this.http.get<Offer[] | []>(`${this.mainPath}${place}`)
-      .subscribe(offers => {
-        this.setOffers(offers);
-      });
-  }
-
-  getOffersByPlaceAndTech(place: string, tech: string) {
-    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}`)
-      .subscribe(offers => {
-        this.setOffers(offers);
-      });
-  }
-
-  getOffersByExp(place: string, tech: string, exp: string) {
-    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}`)
-      .subscribe(offers => {
-        this.setOffers(offers);
-      });
-  }
-
-  getOffersByMinSal(place: string, tech: string, exp: string, minSal: string) {
-    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}/${minSal}`)
-      .subscribe(offers => {
-        this.setOffers(offers);
-      });
-  }
-
-  getOffersByAllFilters(place: string, tech: string, exp: string, minSal: string, maxSal: string) {
-    return this.http.get<Offer[] | []>(`${this.mainPath}${place}/${tech}/${exp}/${minSal}/${maxSal}`)
-      .subscribe(offers => {
-        this.setOffers(offers);
-      });
-  }
-
-  private setOffers(offers: Offer[] | []) {
-    this.offers = offers;
-    if (this.offers.length > 0) {
-      this.offersSubject.next([...this.offers]);
+  getOffersByFilter(place?, tech?, exp?, minSal?, maxSal?) {
+    let pathAPI;
+    if (maxSal) {
+      pathAPI = `${this.mainPath}${place}/${tech}/${exp}/${minSal}/${maxSal}`;
     } else {
-      this.offersSubject.next([]);
+      if (minSal) {
+        pathAPI = `${this.mainPath}${place}/${tech}/${exp}/${minSal}`;
+      } else {
+        if (exp) {
+          pathAPI = `${this.mainPath}${place}/${tech}/${exp}`;
+        } else {
+          if (tech) {
+            pathAPI = `${this.mainPath}${place}/${tech}`;
+          } else {
+            if (place) {
+              if (place === 'remote') {
+                pathAPI = `http://localhost:5000/remote`;
+              } else {
+                pathAPI = `${this.mainPath}${place}`;
+              }
+            }
+          }
+        }
+      }
     }
+
+    return this.http.get<Offer[]>(pathAPI).subscribe(
+      offers => {
+        this.offers = offers;
+        this.offersSubject.next(this.offers);
+      },
+      error => {
+        if (error instanceof HttpErrorResponse) {
+          this.errorMessage = error.error.message;
+          this.offersSubject.next([]);
+        }
+      }
+    );
   }
 }
